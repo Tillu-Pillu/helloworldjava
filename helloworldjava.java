38ae2aa0c76dcaa20130aff4d7f7d590a35829e7
rakshitj123 <79228357+rakshitj123@users.noreply.github.com>
2022-03-10 16:12:32 -0500

    Update helloworldjava.java

--- helloworldjava.java
+++ helloworldjava.java
@@ -1,6 +1,6 @@
 public class helloworldjava {
    public static void main(String[] args) {
-    System.out.println("Hello World from DevOps pollSCM9");
+    System.out.println("Hello World from DevOps pollSCM");
     }
 }
 
